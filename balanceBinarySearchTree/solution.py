4cbc47664a389741b0ce21135affc07e99ce2a06
darya.bulanova <d.bulanova@dodopizza.com>
2021-02-24 00:27:15 +0300

    use AVL-tree as solution
    1382. Balance a Binary Search Tree
    
    https://leetcode.com/problems/balance-a-binary-search-tree/

--- balanceBinarySearchTree/solution.py
+++ balanceBinarySearchTree/solution.py
@@ -4,72 +4,60 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-import enum
-from enum import Enum
-
 from core.dataStructure import TreeNode
 
-
-def height(node) -> int:
-    pass
-
-
-def right_heavy(left_height, right_height):
-    return right_height > left_height + 1
-
-
-def left_heavy(left_height, right_height):
-    return left_height > right_height + 1
-
-
-class Balance(enum.IntEnum):
-    NONE = -1
-    EVEN = 0
-    LEFT_HEAVY = 1
-    RIGHT_HEAVY = 2
-
 class Solution:
-    def balanceBST(self, root: TreeNode) -> TreeNode:
-        return self.balance(root)[0]
 
-    def balance(self, node: TreeNode) -> (TreeNode, Balance, int):
+    heights = dict()
+
+    def set_height(self, node: TreeNode, height: int):
+        self.heights[node.val] = height
+
+    def get_height(self, node: TreeNode) -> int:
         if node is None:
-            return None, Balance.NONE, -1
+            return -1
 
-        if node.left is None and node.right is None:
-            return node, Balance.EVEN, 0
+        return self.heights[node.val]
 
-        node.left, left_balance, left_height = self.balance(node.left)
-        node.right, right_balance, right_height = self.balance(node.right)
+    def update_height(self, node):
+        self.heights[node.val] = max(self.get_height(node.right), self.get_height(node.left)) + 1
 
-        if left_height == right_height:
-            return node, Balance.EVEN, left_height+1
+    def right_heavy(self, node):
+        return self.get_height(node.right) > self.get_height(node.left) + 1
 
-        if left_height == right_height + 1:
-            return node, Balance.LEFT_HEAVY, left_height + 1
+    def left_heavy(self, node):
+        return self.get_height(node.left) > self.get_height(node.right) + 1
 
-        if right_height == left_height + 1:
-            return node, Balance.RIGHT_HEAVY, right_height + 1
+    def balanceBST(self, root: TreeNode) -> TreeNode:
+        return self.rebalance(root)
 
-        if right_heavy(left_height, right_height):
-            if right_balance == Balance.LEFT_HEAVY:
+    def rebalance(self, node: TreeNode) -> TreeNode:
+        if node is None:
+            return None
+
+        node.left = self.rebalance(node.left)
+        node.right = self.rebalance(node.right)
+
+        if self.right_heavy(node):
+            if self.get_height(node.right.left) > self.get_height(node.right.right):
                 node.right = self.right_rotate(node.right)
-            return self.left_rotate(node), Balance.EVEN, right_height
-        if left_heavy(left_height, right_height):
-            if left_balance == Balance.RIGHT_HEAVY:
+            node = self.rebalance(self.left_rotate(node))
+        elif self.left_heavy(node):
+            if self.get_height(node.left.right) > self.get_height(node.left.left):
                 node.left = self.left_rotate(node.left)
-            return self.right_rotate(node), Balance.EVEN, left_height
+            node = self.rebalance(self.right_rotate(node))
+
+        self.update_height(node)
+        return node
 
     def left_rotate(self, node: TreeNode) -> TreeNode:
         root = node.right
-        node.right = None
         node.right = root.left
         root.left = node
-        return self.balance(root)[0]
+        return root
 
-    def right_rotate(self, node) -> TreeNode:
+    def right_rotate(self, node: TreeNode) -> TreeNode:
         root = node.left
-        node.left = None
         node.left = root.right
         root.right = node
-        return self.balance(root)[0]
+        return root
