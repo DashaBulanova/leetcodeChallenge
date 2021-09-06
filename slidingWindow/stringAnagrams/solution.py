e8d12fdab1beb55aa2630fceb40088abb32aeab0
darya.bulanova <darya.bulanova@gmail.com>
2021-09-06 13:43:39 +0300

    educative.io: String Anagrams (hard)
    fix bug

--- slidingWindow/stringAnagrams/solution.py
+++ slidingWindow/stringAnagrams/solution.py
@@ -1,8 +1,8 @@
 class Solution:
     def find_string_anagrams(self, str, pattern):
         pattern_map = dict()
-        for p in pattern:
-            pattern_map[p] = pattern_map.get(p, 0) + 1
+        for ch in pattern:
+            pattern_map[ch] = pattern_map.get(ch, 0) + 1
 
         window_start = 0
         matched = 0
@@ -10,16 +10,16 @@ class Solution:
 
         for window_end in range(len(str)):
             if str[window_end] in pattern_map:
-                if pattern_map[str[window_end]] != 0:
-                    matched += 1
                 pattern_map[str[window_end]] -= 1
+                if pattern_map[str[window_end]] == 0:
+                    matched += 1
 
-            if matched == len(pattern):
+            if matched == len(pattern_map):
                 result.append(window_start)
 
             if window_end - window_start + 1 >= len(pattern):
                 if str[window_start] in pattern_map:
-                    if pattern_map[str[window_start]] >= 0:
+                    if pattern_map[str[window_start]] == 0:
                         matched -= 1
                     pattern_map[str[window_start]] += 1
                 window_start += 1
