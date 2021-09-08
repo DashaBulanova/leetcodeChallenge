d7cea1fa3ce412512faf57ad495b277970a66139
darya.bulanova <darya.bulanova@gmail.com>
2021-09-08 14:16:44 +0300

    educative.io: Remove Duplicates (easy) leetcode: 26. Remove Duplicates from Sorted Array (https://leetcode.com/problems/remove-duplicates-from-sorted-array/)
    small improvement

--- twopointers/deleteDuplicates/solution.py
+++ twopointers/deleteDuplicates/solution.py
@@ -3,21 +3,15 @@ from typing import List
 
 class Solution:
     def remove_duplicates(self, arr: List[int]):
-        result = 1
         last_unique_index = 0
         check_index = 1
 
         while check_index < len(arr):
-            check_item = arr[check_index]
-            last_unique_item = arr[last_unique_index]
-
-            if last_unique_item != check_item:
-                if check_index != last_unique_index+1:
-                    arr[last_unique_index+1] = check_item
+            if arr[last_unique_index] != arr[check_index]:
+                if check_index != last_unique_index + 1:
+                    arr[last_unique_index + 1] = arr[check_index]
                     arr[check_index] = -1
                 last_unique_index += 1
-                result += 1
             check_index += 1
 
-        return result
-
+        return last_unique_index + 1
