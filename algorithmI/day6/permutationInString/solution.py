b6c6464f993323a514462cac22ac5a2cba8b5596
darya.bulanova <darya.bulanova@gmail.com>
2021-11-05 11:46:51 +0300

    567. Permutation in String
    add time complexity and space complexity

--- algorithmI/day6/permutationInString/solution.py
+++ algorithmI/day6/permutationInString/solution.py
@@ -1,11 +1,16 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
         needs = {}
         for i in s1:
             needs[i] = needs[i]+1 if i in needs else 1
 
         start = 0
         total_needs = len(s1)
+
+#Space complexity: in worst case 26 in best case len(s1): O(1)
+#Time complexity: O(len(s1))+O(2*len(s2))=O(l1+l2-l1)
 
         for end in range(len(s2)):
             if s2[end] in needs:
