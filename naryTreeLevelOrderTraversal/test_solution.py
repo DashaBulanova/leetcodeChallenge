d20ea65f3e1fe3cc06cbb63de0eae5a6c2c83f75
darya.bulanova <darya.bulanova@gmail.com>
2021-11-17 15:01:05 +0300

    234. Palindrome Linked List (easy) Palindrome LinkedList (medium)#
    https://leetcode.com/problems/palindrome-linked-list/
    https://www.educative.io/courses/grokking-the-coding-interview/JERG3q0p912

--- naryTreeLevelOrderTraversal/test_solution.py
+++ naryTreeLevelOrderTraversal/test_solution.py
@@ -3,8 +3,27 @@ from .solution import Solution
 
 
 def test_number_of_bits():
-    root = Node(val=1)
-    root.children = [Node(val=3, children=[Node(val=5), Node(val=6)]), Node(val=2), Node(val=4)]
+    import os
+    from collections import deque
 
-    actual = Solution().levelOrder(root)
-    assert actual == [[1], [3, 2, 4], [5, 6]]
+    FOLDER = "/Users/darya.bulanova/Downloads"
+    files_total = 0
+    dirs_total = 0
+    queue = deque([FOLDER])
+    while queue:
+        dir_ = queue.popleft()
+        files = os.listdir(dir_)
+        for file in files:
+            if file.endswith("/"):
+                queue.append(file)
+                dirs_total += 1
+            else:
+                if file.endswith('.parquet'):
+                    files_total += 1
+    print(f"dirs files={dirs_total}")
+    print(f"total files={files_total}")
+    # root = Node(val=1)
+    # root.children = [Node(val=3, children=[Node(val=5), Node(val=6)]), Node(val=2), Node(val=4)]
+    #
+    # actual = Solution().levelOrder(root)
+    # assert actual == [[1], [3, 2, 4], [5, 6]]
