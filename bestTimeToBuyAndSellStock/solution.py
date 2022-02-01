a5cb0b62d8386f676b6dc1a338c02495bb962482
DashaBulanova <darya.bulanova@gmain.com>
2022-02-01 14:40:18 +0300

    121. Best Time to Buy and Sell Stock (easy)

--- bestTimeToBuyAndSellStock/solution.py
+++ bestTimeToBuyAndSellStock/solution.py
@@ -3,13 +3,10 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        if len(prices) == 0:
-            return 0
-
-        max_profit = 0
+        profit = 0
         min_price = prices[0]
+        for i in range(1, len(prices)):
+            min_price = min(prices[i-1], min_price)
+            profit = max(profit, prices[i] - min_price)
 
-        for i in prices:
-            max_profit = max(prices[i] - min_price, max_profit)
-            min_price = min(prices[i], min_price)
-        return max_profit
+        return profit
--- bestTimeToBuyAndSellStock/test_solution.py
+++ bestTimeToBuyAndSellStock/test_solution.py
@@ -1,11 +1,34 @@
 import pytest
+from .solution import Solution
 
-from bestTimeToBuyAndSellStock.solution import Solution
-
-
-@pytest.mark.parametrize("input, expected",
-                         [([7, 1, 5, 3, 6, 4], 5),
-                          ([7, 6, 4, 3, 1], 0)])
-def test_number_of_bits(input, expected):
-    actual = Solution().maxProfit(input)
+@pytest.mark.parametrize("prices, expected",[
+([7,1,5,3,6,4], 5),
+([7,6,4,3,1], 0)
+    ])
+def test(prices, expected):
+    actual = Solution().maxProfit(prices)
     assert actual == expected
+
+
+"""
+[7,1,5,3,6,4] 
+profit = 0
+min_price = 7
+
+i=1
+min_price = 7
+profit = max(0, -6) = 0
+
+i=2
+min_price = 1
+profit = max(0, 5-1=4)=4
+
+i=3
+min_price=1
+profit = max(4, 2) = 4
+
+i=4
+min_price=1
+profit = max(4, 6-1) = 5
+
+"""
