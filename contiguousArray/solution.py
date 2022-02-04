8321283aa68674b2c232263d0a50b1a1f30ad2f0
DashaBulanova <darya.bulanova@gmain.com>
2022-02-04 19:15:03 +0300

    525. Contiguous Array (medium) hashtable approach

--- contiguousArray/solution.py
+++ contiguousArray/solution.py
@@ -1,20 +1,15 @@
 from typing import List
 
-
-class Solution(object):
-    #brute force approach
+class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
+        sums = {0:-1}
+        acc = 0
+        result = 0
+        for i in range(len(nums)):
+            acc += 1 if nums[i] == 0 else -1
+            if acc in sums:
+                result = max(result, i - sums[acc])
+            else:
+                sums[acc]=i
 
-        max_size = 0
-
-        for i in range(0, len(nums)):
-            sum = -1 if nums[i] == 0 else 1
-
-            for j in range(i + 1, len(nums)):
-                add = -1 if nums[j] == 0 else 1
-                sum += add
-
-                if sum == 0:
-                    max_size = max(max_size, j - i + 1)
-
-        return max_size
+        return result
--- contiguousArray/test_solution.py
+++ contiguousArray/test_solution.py
@@ -1,14 +1,56 @@
 import pytest
+from .solution import Solution
 
-from contiguousArray.solution import Solution
-
-
-@pytest.mark.parametrize("input, expected",
-                         [([0, 1], 2),
-                          ([0, 1, 0], 2),
-                          ([0, 1, 1, 0], 4),
-                          ([0, 1, 1, 0, 1, 1, 1, 0], 4),
-                          ([0, 0, 1, 0, 0, 0, 1, 1], 6)])
-def test_number_of_bits(input, expected):
+@pytest.mark.parametrize("input, expected",[
+([0,1], 2),
+([0,1,0], 2),
+([0,0,0], 0),
+([0,1,1,0,1,1,1,0], 4)
+])
+def test(input, expected):
     actual = Solution().findMaxLength(input)
     assert actual == expected
+"""
+[0,1,1,0,1,1,1,0]
+
+i=0
+acc=0+1=1
+sums={0:-1, 1:0}
+
+i=1
+acc = 1-1=0
+result=2
+sums={0:-1, 1:0}
+
+i=2
+acc=-1
+sums={0:-1, 1:0, -1:2}
+
+i=3
+acc=0
+sums={0:-1, 1:0, -1:2}
+result=4
+
+i=4
+acc=-1
+sums={0:-1, 1:0, -1:2}
+4-2=2
+result=4
+
+i=5
+acc=-2
+sums={0:-1, 1:0, -1:2, -2:5}
+result=4
+
+
+i=6
+acc=-3
+sums={0:-1, 1:0, -1:2, -2:5, -3:6}
+result=4
+
+
+i=7
+acc=-2
+sums={0:-1, 1:0, -1:2, -2:5}
+result=4
+"""
