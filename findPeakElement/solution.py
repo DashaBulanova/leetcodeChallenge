3da8d93e9c55a57a9525eace026a86ca3ef6c988
DashaBulanova <darya.bulanova@gmain.com>
2022-02-09 11:38:12 +0300

    162. Find Peak Element (medium)

--- findPeakElement/solution.py
+++ findPeakElement/solution.py
@@ -3,13 +3,16 @@ from typing import List
 
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-        return self.findPeak(nums, 0, len(nums)-1)
 
-    def findPeak(self, nums: List[int], start: int, end: int):
-        mid = start + int((end - start)/2)
-        if (mid + 1 <= len(nums) - 1) and nums[mid] < nums[mid + 1]:
-            return self.findPeak(nums, mid + 1, end)
-        elif mid - 1 >= 0 and nums[mid] < nums[mid - 1]:
-            return self.findPeak(nums, start, mid - 1)
-        else:
-            return nums[mid]
+        def findPeak(s, e) -> int:
+            mid = s + (e - s) // 2
+            mid_item = nums[mid]
+
+            if mid != 0 and mid_item < nums[mid-1]:
+                return findPeak(s, mid)
+            if mid < len(nums) - 1 and mid_item < nums[mid+1]:
+                return findPeak(mid + 1, e)
+            else:
+                return mid
+
+        return findPeak(0, len(nums) - 1)
