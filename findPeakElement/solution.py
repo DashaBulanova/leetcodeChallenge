9d6a1287031ef9e4635087c13b859b5763330953
DashaBulanova <darya.bulanova@gmain.com>
2022-02-14 13:42:22 +0300

    162. Find Peak Element (medium)

--- findPeakElement/solution.py
+++ findPeakElement/solution.py
@@ -1,18 +1,16 @@
 from typing import List
 
-
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
+        if len(nums)==1:
+            return nums[0]
 
-        def findPeak(s, e) -> int:
-            mid = s + (e - s) // 2
-            mid_item = nums[mid]
-
-            if mid != 0 and mid_item < nums[mid-1]:
-                return findPeak(s, mid)
-            if mid < len(nums) - 1 and mid_item < nums[mid+1]:
-                return findPeak(mid + 1, e)
+        s,e=0,len(nums)-1
+        while s<=e:
+            mid=s+(e-s)//2
+            if mid<len(nums)-1 and nums[mid]<nums[mid+1]:
+                s=mid+1
+            elif mid>0 and nums[mid-1]>nums[mid]:
+                e=mid
             else:
                 return mid
-
-        return findPeak(0, len(nums) - 1)
--- findPeakElement/test_solution.py
+++ findPeakElement/test_solution.py
@@ -1,32 +1,11 @@
 import pytest
-
 from .solution import Solution
 
-
-@pytest.mark.parametrize("input, expected", [
-    ([1, 2, 3, 1], 2),
-    ([1, 2, 1, 3, 5, 6, 4], 5),
-    ([1], 0),
-    ([1, 2], 1)
+@pytest.mark.parametrize("input, expected",[
+([1,2,3,1], 2),
+([1,2,1,3,5,6,4], 5),
+([1,2],1)
 ])
 def test(input, expected):
     actual = Solution().findPeakElement(input)
     assert actual == expected
-
-
-"""
-1,2,3,1 
-s=0 e=3
-mid=0+3//2=1
-1<2>3 False
-
-left = find(0,1) = None
-    mid=0 
-    mid_item = 1
-    1 > 2 False
-rigth = find(2,3)
-    mid=2
-    mid_item=3
-    
-
-"""
