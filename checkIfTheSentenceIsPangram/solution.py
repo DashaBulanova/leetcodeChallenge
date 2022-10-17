3193c9f4ba7bbd7707d14e8782d4468860b3b342
Dasha Bulanova <daria.bulanova@hellofresh.com>
2022-10-17 14:16:33 +0300

    create set directly from list.
     Daily LeetCoding Challenge October, Day 17
     1832. Check if the Sentence Is Pangram
    (easy)
    
    https://leetcode.com/problems/check-if-the-sentence-is-pangram

--- checkIfTheSentenceIsPangram/solution.py
+++ checkIfTheSentenceIsPangram/solution.py
@@ -1,7 +1,3 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
-        alphabet = set()
-        for c in sentence:
-            if c not in alphabet:
-                alphabet.add(c)
-        return len(alphabet) == 26
+        return len(set(sentence)) == 26
