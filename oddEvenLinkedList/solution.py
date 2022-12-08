7a64a162389624020a8f1882e1bd2f4d50b042a8
Dasha Bulanova <daria.bulanova@hellofresh.com>
2022-12-08 15:33:52 +0100

    Daily LeetCoding Challenge December, Day 8 872. Leaf-Similar Trees O(1)
    Time taken: 27 m 46 s
    Next question
    873. Length of Longest Fibonacci Subsequence
    More challenges
    663. Equal Tree Partition
    666. Path Sum IV
    2236. Root Equals Sum of Children
    
    https://leetcode.com/problems/leaf-similar-trees

--- oddEvenLinkedList/solution.py
+++ oddEvenLinkedList/solution.py
@@ -1,64 +1,56 @@
 # Definition for singly-linked list.
-from typing import List
-
-
 class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
-    def __str__(self):
-        head = self
-        current_node = head
-        result = ""
-        while current_node is not None:
-            if result != '':
-                result += '->'
-            result += str(current_node.val)
-            current_node = current_node.next
 
-        result += '->NULL'
-        return result
+from typing import Optional
 
 
 class Solution:
-    # idea:
-    # 1. Count the length of the linked list
-    # 2. Пройти еще раз по linked list и менять на ходу элементы
-    def oddEvenList(self, head: ListNode) -> ListNode:
-        length = self.get_length(head)
-
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         curr = head
-        prev = None
-
+        result = curr
         last_odd = None
-        first_even = None
+        index = 1
+        while curr:
+            if index % 2 == 0:
+                p = curr
+                n=curr.next
+                p.next = curr.next.next
+                n.next=p
 
-        for i in range(1, length + 1):
-            if i % 2 != 0:
-                if last_odd is None:
-                    last_odd = curr
-                else:
-                    tail = curr.next
-                    last_odd.next = curr
-                    prev.next = tail
-                    curr.next = first_even
-
-                    last_odd = curr
-                    curr = prev
+                curr.next.next = None
             else:
-                if first_even is None:
-                    first_even = curr
-            prev = curr
+                c = last_odd.next
+                c.next = None
+                prev_odd.next = curr
+                curr.next = c
+                prev_odd = curr
             curr = curr.next
+            index += 1
+        return result
 
-        return head
 
-    def get_length(self, head):
-        count = 0
-        node = head
+"""
+   |
+1->2->3->4->5
+1->2
+n=3->4->5
+p=2
+p=2-4-5
+n=3-2-4-5
+1->3->2->4->5
+#1->2->3->4->5
+curr = 1->2->3->4->5
+prev_od = None
 
-        while node is not None:
-            count += 1
-            node = node.next
-        return count
+prev_od = 1->2->3->4->5
+curr=2->3->4->5
+
+p=1->2->3->4->5
+c=2
+p=1
+
+"""
--- oddEvenLinkedList/test_solution.py
+++ oddEvenLinkedList/test_solution.py
@@ -1,44 +1,16 @@
-from typing import List
-
 import pytest
 
-from oddEvenLinkedList.solution import Solution, ListNode
+from .solution import Solution, ListNode
 
-
-def to_linked_list(input: List) -> ListNode:
-    head = None
-    current_node = None
-
-    for i in input:
-        if head is None:
-            head = ListNode(i)
-            current_node = head
-        else:
-            current_node.next = ListNode(i)
-            current_node = current_node.next
-
-    return head
-
-
-def to_list(head: ListNode):
-    result = []
-    curr = head
-    while curr is not None:
-        result.append(curr.val)
-        curr = curr.next
-
-    return result
-
-
-@pytest.mark.parametrize("input, expected",
-                         [
-                             ([1, 2, 3, 4, 5], [1, 3, 5, 2, 4]),
-                             ([2, 1, 3, 5, 6, 4, 7], [2, 3, 6, 7, 1, 5, 4]),
-
-                         ])
-def test_solution(input, expected):
-    head = to_linked_list(input)
-    i = str(head)
+def test():
+    head = ListNode(1)
+    head.next = ListNode(2)
+    head.next.next = ListNode(3)
+    head.next.next.next = ListNode(4)
+    head.next.next.next.next = ListNode(5)
     actual = Solution().oddEvenList(head)
-
-    assert to_list(actual) == expected
+    assert actual.val == 1
+    assert actual.next.val == 3
+    assert actual.next.next.val == 5
+    assert actual.next.next.next.val == 2
+    assert actual.next.next.next.next.val == 4
