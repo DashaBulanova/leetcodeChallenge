e6036dbc995512bd57a209b66b85a92460b6d7e8
Dasha Bulanova <daria.bulanova@hellofresh.com>
2023-10-06 00:02:31 +0200

    229. Majority Element II https://leetcode.com/problems/majority-element-ii/?envType=daily-question&envId=2023-10-05

--- majorityElementII/solution.py
+++ majorityElementII/solution.py
@@ -3,19 +3,11 @@ from typing import List
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-
-        required = int(len(nums)/3)
-        nums = sorted(nums)
         result = set()
-        prev = None
-        count = 0
-        for n in nums:
-            if prev is None or prev != n:
-                prev = n
-                count = 0
-            if prev == n:
-                count += 1
-                if count > required:
-                    result.add(n)
-
+        times = len(nums) / 3
+        map = {}
+        for i in range(len(nums)):
+            map[nums[i]] = map.get(nums[i], 0) + 1
+            if map[nums[i]] > times:
+                result.add(nums[i])
         return list(result)
