2dfa4777eaf7134a13a6686d4a5e7a61ec9f28cb
Dasha Bulanova <daria.bulanova@hellofresh.com>
2024-01-08 18:34:12 +0100

    938. Range Sum of BST
    Daily Challenge [08.01.2024]
    
    https://leetcode.com/problems/range-sum-of-bst/description/

--- rangeSumOfBST/solution.py
+++ rangeSumOfBST/solution.py
@@ -1,30 +1,23 @@
-
 # Definition for a binary tree node.
-from typing import Optional
-
-
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
-
-
-def _calculate_sum(node, low, high) -> int:
-    if node is None:
-        return 0
-
-    left_sum = _calculate_sum(node.left, low, high)
-    right_sum = _calculate_sum(node.right, low, high)
-
-    if low <= node.val <= high:
-        return left_sum + node.val + right_sum
-    else:
-        return left_sum+right_sum
-
-
-
 class Solution:
-    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
-        return _calculate_sum(root, low, high)
+    def rangeSumBST(self, root: TreeNode | None, low: int, high: int) -> int:
+        return self._sum(root, low, high)
+    def _sum(self, node:TreeNode, low, high) -> int:
+        if node is None:
+            return 0
 
+        result = 0
+        if node.val < high:
+            result += self._sum(node.right, low, high)
+        if node.val > low:
+            result += self._sum(node.left, low, high)
+
+        if low <= node.val <= high:
+            result +=  node.val
+
+        return result
--- rangeSumOfBST/test.py
+++ rangeSumOfBST/test.py
@@ -1,38 +1,35 @@
-import collections
-
 import pytest
-
-from core.exceptions.constraints_violation import ConstraintsViolationException
+from collections import deque
 from .solution import Solution, TreeNode
 
 
-def from_array(input: list[int]) -> TreeNode:
-    if not input:
-        raise ConstraintsViolationException()
+def _to_tree(input: list[int | None]) -> TreeNode:
+    if not input or input[0] is None:
+        raise ValueError()
 
-    q = collections.deque()
-    root = None
-    for i in range(len(input)):
-        curr = TreeNode(input[i]) if input[i] else None
-        if i == 0:
-            root = curr
-        elif i % 2 == 0:
-            node = q.popleft()
-            node.right = curr
-        else:
-            node = q[0]
-            node.left = curr
+    root = TreeNode(input[0])
+    q = deque([root])
 
-        if curr:
-            q.append(curr)
+    for i in range(1, len(input)):
+        new = TreeNode(input[i])
+        if input[i] is not None:
+            if i % 2 == 0:
+                q[0].right = new
+            else:
+                q[0].left = new
+        if input[i] is not None:
+            q.append(new)
+        if i % 2 == 0:
+            q.popleft()
+
     return root
 
 
 @pytest.mark.parametrize("input, low, high, expected", [
-    ([10, 5, 15, 3, 7, None, 18], 7, 15, 32),
+    ([10,5,15,3,7,None,18],7, 15, 32),
     ([10, 5, 15, 3, 7, 13, 18, 1, None, 6], 6, 10, 23)
 ])
 def test(input, low, high, expected):
-    root = from_array(input)
+    root = _to_tree(input)
     actual = Solution().rangeSumBST(root, low, high)
     assert actual == expected
