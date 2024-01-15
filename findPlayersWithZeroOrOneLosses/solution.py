f7a004b79c6d42ca6d9fc5dba2fe8bf67af458f6
Dasha Bulanova <daria.bulanova@hellofresh.com>
2024-01-15 12:24:27 +0100

    2225. Find Players With Zero or One Losses \n Daily Challenge: Jan 2024

--- findPlayersWithZeroOrOneLosses/solution.py
+++ findPlayersWithZeroOrOneLosses/solution.py
@@ -1,25 +1,12 @@
-from typing import List
-
-
 class Solution:
-    def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
-        # identifier of player: [0, 1] 0- count of winning games 1 - number of losing games
-        players = {}
-
+    def findWinners(self, matches: list[list[int]]) -> list[list[int]]:
+        winners = {}
+        all_losers = {}
         for match in matches:
-            if match[0] not in players:
-                players[match[0]] = 0
-            if match[1] in players:
-                players[match[1]] += 1
-            else:
-                players[match[1]] = 1
+            winners[match[0]] = winners.get(match[0], 0) + 1
+            all_losers[match[1]] = all_losers.get(match[1],0) + 1
 
-        result_winner = []
-        result_loser = []
-        for i in sorted(players.keys()):
-            if players[i] == 0:
-                result_winner.append(i)
-            if players[i] == 1:
-                result_loser.append(i)
-
-        return [result_winner, result_loser]
+        return [
+            sorted(set(winners.keys())-set(all_losers.keys())), 
+            sorted([k for k,v in all_losers.items() if v == 1])
+        ]
