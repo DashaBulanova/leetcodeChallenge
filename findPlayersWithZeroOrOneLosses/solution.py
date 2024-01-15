2f2662f80c0796e02cb226f83ba34c73582c81ba
Dasha Bulanova <daria.bulanova@hellofresh.com>
2024-01-15 12:39:45 +0100

    simplify solution using balance with only one dict

--- findPlayersWithZeroOrOneLosses/solution.py
+++ findPlayersWithZeroOrOneLosses/solution.py
@@ -1,12 +1,11 @@
 class Solution:
     def findWinners(self, matches: list[list[int]]) -> list[list[int]]:
-        winners = {}
-        all_losers = {}
+        players_balance = {}
         for match in matches:
-            winners[match[0]] = winners.get(match[0], 0) + 1
-            all_losers[match[1]] = all_losers.get(match[1],0) + 1
+            players_balance[match[0]] = players_balance.get(match[0],0)
+            players_balance[match[1]] = players_balance.get(match[1],0) - 1
 
         return [
-            sorted(set(winners.keys())-set(all_losers.keys())), 
-            sorted([k for k,v in all_losers.items() if v == 1])
+            sorted([k for k,v in players_balance.items() if v == 0]), 
+            sorted([k for k,v in players_balance.items() if v == -1])
         ]
