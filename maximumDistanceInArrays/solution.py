47e06695b57f006cf1f64c44ad28434cd7982990
Dasha Bulanova <daria.bulanova@hellofresh.com>
2024-08-16 17:01:21 +0200

    improvement: time 0(N) & space 0(1)

--- maximumDistanceInArrays/solution.py
+++ maximumDistanceInArrays/solution.py
@@ -1,16 +1,12 @@
+import math
+
+
 class Solution:
     def maxDistance(self, arrays: list[list[int]]) -> int:
-        m = len(arrays)
-        maxs = []
-        mins = []
+        res = 0
+        min_, max_ = math.inf, -math.inf
         for i in range(len(arrays)):
-            mins.append((arrays[i][0], i))
-            maxs.append((arrays[i][len(arrays[i]) - 1], i))
-
-        mins.sort()
-        maxs.sort(reverse=True)
-
-        if mins[0][1] != maxs[0][1]:
-            return abs(mins[0][0] - maxs[0][0])
-        else:
-            return max(abs(mins[0][0] - maxs[1][0]), abs(mins[1][0] - maxs[0][0]))
+            res = max(res, arrays[i][-1] - min_, max_ - arrays[i][0])
+            min_ = min(min_, arrays[i][0])
+            max_ = max(max_, arrays[i][-1])
+        return res
